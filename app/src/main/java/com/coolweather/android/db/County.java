037e851f60c6b56ada15da538cc893edb0c8cd2f
Ael <aeling233@gmail.com>
2019-11-08 19:17:02 +0800

    创建数据库以及各种配置。

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,42 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countryName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
